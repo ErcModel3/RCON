6ace2378f6b8b21751e502739ca92f2b5687b4a2
Alex Kirk <alexanderjoshua21@protonmail.com>
2026-01-05 16:58:04 +0000

    Pushing changes for alex

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,27 @@ https://www.geeksforgeeks.org/python/flask-blueprints/
 https://flask.palletsprojects.com/en/stable/quickstart/#routing
 """
 
+from flask import Blueprint, request
+
+rcon = Blueprint('blueprint', __name__)
+
+@rcon.get('/')
+def index():
+    return "Index page"
+
+# Combined html api requests should be done using rcon.route()
+@rcon.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method=='GET':
+        return get_login()
+    else:
+        return post_login()
+
+@rcon.post('logout')
+def logout():
+    return "logout page"
+
+@rcon.route('/hello')
 def hello():
     """
     First route as a guideline
@@ -15,4 +36,4 @@ def get_login():
     return "get login"
     
 def post_login():
-    return "get login"
+    
